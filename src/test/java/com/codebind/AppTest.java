d73af97f34a3e08e25747b17b024b94520a24f00
gerald00 <ghc47538067@gmail.com>
2017-02-13 13:20:30 +0000

    sort of commit message

---
+++ src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,22 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+    
+	/*protected int value1, value2;
+	 protected void setUp(){
+		 
+	      value1 = 3;
+	      value2 = 3;
+	   }
+
+	   @Test
+	 // test method to add two values
+	   public void testAdd(){
+	      double result = value1 + value2;
+	      assertTrue(result == 6);
+	   }*/
+	}
